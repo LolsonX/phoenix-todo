2eadcf463cd0eef2aeec9404cf0c6615caa06923
LolsonX <xlolsonx@gmail.com>
2022-08-23 20:09:16 +0200

    Add flash messages

--- lib/todo_web/controllers/items_controller.ex
+++ lib/todo_web/controllers/items_controller.ex
@@ -12,16 +12,22 @@ defmodule TodoWeb.ItemsController do
 
   def create(conn, %{"item" => item_params}) do
     Items.create_item(item_params)
-    redirect(conn, to: "/")
+    conn
+    |> put_flash(:info, "Added item!")
+    |> redirect(to: "/")
   end
 
   def complete(conn, %{"id" => id}) do
     Items.mark_completed(id)
-    redirect(conn, to: "/")
+    conn
+    |> put_flash(:success, "Completed!")
+    |> redirect(to: "/")
   end
 
   def delete(conn, %{"id" => id}) do
     Items.delete_item(id)
-    redirect(conn, to: "/")
+    conn
+    |> put_flash(:error, "Deleted")
+    |> redirect(to: "/")
   end
 end
